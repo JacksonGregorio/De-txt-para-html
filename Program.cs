6eeb627f9b499a06fd4b6016228877b3fb5a6e1b
Everson Pereira <everson.pereira.ifsp@gmail.com>
2023-06-11 15:53:35 -0300

    fix: text, combobox and button

--- Program.cs
+++ Program.cs
@@ -3,33 +3,50 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace projeto_nelson
 {
     internal class Program
     {
+        static string s = "cod:space";
+
         static string returnLineHtml(string content, string typeTag) {
-            string startTag = "";
-            string endTag = "";
+            string linhaHTML = "";
 
-            if(typeTag == "button"){
-                startTag = "<button>";
-                endTag = "</button>";
-            } else if(typeTag == "label"){
-                startTag = "<label>";
-                endTag = "</label>";
-            } else {
-                startTag = "<p>";
-                endTag = "</p>";
-            }
-
-            string linhaHTML = System.Text.RegularExpressions.Regex.Replace(content, $@"{typeTag}:(.*)", match =>
+            if(typeTag == "button")
+                linhaHTML = System.Text.RegularExpressions.Regex.Replace(content, @"button:(\w+)", $@"<input{s}type=""submit""{s}title=""$1"">");
+            else if(typeTag == "text")
+                linhaHTML = System.Text.RegularExpressions.Regex.Replace(content, @"text:(\w+)", $@"$1:{s}<input{s}type=""text"">");
+            else if(typeTag == "combobox")
+            {
+                string pattern = @"combobox:\[(.*?)\]";
+    
+                linhaHTML = Regex.Replace(content, pattern, match =>
                 {
-                    string contentTag = match.Groups[1].Value.Trim();
-                    return startTag + contentTag + endTag;
+                    string optionsStr = match.Groups[1].Value;
+                    string[] options = optionsStr.Split(',');
+                    
+                    string selectContent = "<select>";
+                    foreach (string option in options)
+                    {
+                        string[] parts = option.Split(':');
+                        if (parts.Length == 2)
+                        {
+                            selectContent += "  <option>" + parts[1].Trim() + "</option>";
+                        }
+                    }
+                    selectContent += "</select>";
+                    
+                    return selectContent;
                 });
-
+            }
+            
             return linhaHTML;
+        }
+
+        static string fixLineSpacement(string lineHtml) {
+            return lineHtml.Replace(" ", "&nbsp;&nbsp;").Replace(s, " ");
         }
 
         static void Main(string[] args)
@@ -55,33 +72,45 @@ namespace projeto_nelson
                     writer.WriteLine("</head>");
                     writer.WriteLine("<body>");
 
+                    writer.WriteLine("<form>");
                     // Iterar sobre as linhas do arquivo TXT
                     foreach (string linha in linhas)
                     {
+                        Console.WriteLine(linha);
                         // Remover espaços em branco no início e no fim da linha
                         string conteudo = linha.Trim();
-
+                        
                         // Verificar se a linha é uma tag de botão
                         if (conteudo.Contains("button:"))
-                        {
-                            writer.WriteLine(returnLineHtml(conteudo, "button"));
-                        }
-                        // Verificar se a linha é uma tag de rótulo
-                        else if (conteudo.Contains("label:"))
-                        {
-                           writer.WriteLine(returnLineHtml(conteudo, "label"));
-                        }
-                        // Verificar se a linha contém caracteres "_" ou "|"
-                        else if (conteudo.Contains("_") || conteudo.Contains("|"))
-                        {
-                            // Gerar a tag de parágrafo no HTML com os caracteres especiais
-                            writer.WriteLine("<p>" + conteudo + "</p>");
-                        }
+                            conteudo = returnLineHtml(conteudo, "button"); 
 
-                        writer.WriteLine("  <br>");
+                            
+                        // Verificar se a linha é uma tag de rótulo
+                        
+                        if (conteudo.Contains("label:"))
+                            conteudo = returnLineHtml(conteudo, "label"); 
+                        
+                        if (conteudo.Contains("text:"))
+                            conteudo = returnLineHtml(conteudo, "text"); 
+
+                           
+                        if (conteudo.Contains("combobox:"))
+                            conteudo = returnLineHtml(conteudo, "combobox"); 
+
+                        
+                        if (conteudo.Contains("radio:"))
+                            conteudo = returnLineHtml(conteudo, "radio"); 
+                            
+                        
+                        if (conteudo.Contains("checkbox:"))
+                            conteudo = returnLineHtml(conteudo, "checkbox"); 
+                           
+                        writer.WriteLine(fixLineSpacement(conteudo));
+                        writer.WriteLine("<br>");
                     }
 
                     // Escrever o fim do documento HTML
+                    writer.WriteLine("</form>");
                     writer.WriteLine("</body>");
                     writer.WriteLine("</html>");
                 }
