67eadea0316cbeb91caf03423e0118b80fb4db79
Everson Pereira <everson.pereira.ifsp@gmail.com>
2023-06-11 20:40:03 -0300

    fix: radio buttons, select and checkbox

--- Program.cs
+++ Program.cs
@@ -3,58 +3,96 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace projeto_nelson
 {
     internal class Program
     {
+        // Variavel utilizada para posteriormente ser trocada por espaço dentro das tags.
         static string s = "cod:space";
 
-        static string returnLineHtml(string content, string typeTag) {
-            string linhaHTML = "";
+        // Função que gera um name aleatorio, especificamente, para radio buttons para a identificação do grupo que faz parte.
+        static string generateNameInput(){
+            int length = 10; // Define o comprimento da string aleatória
+            string allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; // Define o alfabeto de caracteres possíveis
+            
+            StringBuilder sb = new StringBuilder(); // Instancia obj de uma classe para manipulação de strings
+            Random random = new Random(); // Instancia obj de uma classe para geração de numeros aleatorios
 
-            if(typeTag == "button")
-                linhaHTML = System.Text.RegularExpressions.Regex.Replace(content, @"button:(\w+)", $@"<input{s}type=""submit""{s}title=""$1"">");
-            else if(typeTag == "text")
-                linhaHTML = System.Text.RegularExpressions.Regex.Replace(content, @"text:(\w+)", $@"$1:{s}<input{s}type=""text"">");
-            else if(typeTag == "combobox")
+            // Looping com base no tamanho definido para a string
+            for (int i = 0; i < length; i++)
             {
-                string pattern = @"combobox:\[(.*?)\]";
-    
-                linhaHTML = Regex.Replace(content, pattern, match =>
-                {
-                    string optionsStr = match.Groups[1].Value;
-                    string[] options = optionsStr.Split(',');
+                int index = random.Next(allowedCharacters.Length); // gera o valor do index de algum dos caracteres permitidos
+                char randomChar = allowedCharacters[index]; // escolhe o caractere permitido com base no index sorteado
+                sb.Append(randomChar); // Concatena o caractere sorteado a string
+            }
+
+            return sb.ToString(); // Converte o objeto da classe StringBuilder para string
+        }
+
+        // Função que retornara a linha html, com base no conteudo passado e o tipo da tag.
+        static string returnLineHtml(string content, string typeTag) {
+            string lineHtml = "";
+
+            if(typeTag == "button") // Caso o tipo da tag for button
+                lineHtml = Regex.Replace(content, @"button:(\w+)", $@"<input{s}type=""button""{s}value=""$1"">");
+            else if(typeTag == "text") // Caso o tipo da tag seja text
+                lineHtml = Regex.Replace(content, @"text:(\w+)", $@"$1:{s}<input{s}type=""text"">");
+            else { // Caso o tipo da tag do inṕut seja selecionavel (combobox/radio/checkbox)
+                
+                // Regex para verificar as options após ":"
+                string pattern = $@"{typeTag}:\[(.*?)\]";
+                
+                lineHtml = Regex.Replace(content, pattern, match =>
+                {   
+                    string optionsStr = match.Groups[1].Value; // Pega as opções dentro de :[options] e retorna uma string.
+                    string[] options = optionsStr.Split(','); // Pega a string e transforma em um array separando onde houver ",".
                     
-                    string selectContent = "<select>";
-                    foreach (string option in options)
-                    {
-                        string[] parts = option.Split(':');
-                        if (parts.Length == 2)
-                        {
-                            selectContent += "  <option>" + parts[1].Trim() + "</option>";
-                        }
+                    string contentTag = ""; // Inicializa conteudo da tag.
+                    string randomName = ""; // Inicializa name aleatorio para input, caso seja uma caixa de seleção.
+
+                    if(typeTag == "combobox") // Se o tipo da tag for combobox, o inicio da tag devera ser <select>
+                        contentTag = "<select>";
+                    else // Se for uma caixa de seleção, chamamos a função que gera o name aleatorio do input pra caixa de seleção
+                        randomName = generateNameInput();
+                    
+                    // Itera array de opções dentro do input
+                    foreach (string option in options){
+                        if(typeTag == "combobox") // Caso seja um combobox, cada opção é marcada com a tag <option>
+                            contentTag += $"<option>{option}</option>";
+                        else // Caso seja radio ou checkbox, cada opção é marcada com input com o type sendo typeTag e o com name gerado aleatoriamente
+                            contentTag += $@"<input{s}type=""{typeTag}""{s}name=""{randomName}"">{s}{option}";
                     }
-                    selectContent += "</select>";
+
+                    if(typeTag == "combobox") contentTag += "</select>"; // Fechamento da tag <select>, caso seja um combobox
                     
-                    return selectContent;
+                    return contentTag; // Retorna o conteudo HTML apos processar as opções da tag.
                 });
             }
             
-            return linhaHTML;
+            return lineHtml; // Retorna linha HTML gerada.
         }
 
+        // Função responsavel por corrigir o espaçamento após gerar as tags ler a linha iterada do txt.
         static string fixLineSpacement(string lineHtml) {
+            /* 
+                Primeiramente troca tudo onde tiver um espaço " " pela entidade responsavel por adicionar
+                espaço no HTML, depois substitui o codigo para espaço dentro das tags (variavel s) por
+                espaço " ". 
+            */
+
             return lineHtml.Replace(" ", "&nbsp;&nbsp;").Replace(s, " ");
         }
 
         static void Main(string[] args)
         {
-            string arquivoTxt = "./public/teste.txt";
+            // Caminho do arquivo de entrada TXT
+            string arquivoTxt = "./public/input.txt";
 
             // Caminho do arquivo de saída HTML
-            string arquivoHtml = "./public/index.html";
+            string arquivoHtml = "./public/output.html";
 
             try
             {
@@ -71,42 +109,36 @@ namespace projeto_nelson
                     writer.WriteLine("<title>Arquivo HTML gerado</title>");
                     writer.WriteLine("</head>");
                     writer.WriteLine("<body>");
-
                     writer.WriteLine("<form>");
+                    
                     // Iterar sobre as linhas do arquivo TXT
                     foreach (string linha in linhas)
                     {
-                        Console.WriteLine(linha);
-                        // Remover espaços em branco no início e no fim da linha
-                        string conteudo = linha.Trim();
+                        string contentLine = linha;
                         
-                        // Verificar se a linha é uma tag de botão
-                        if (conteudo.Contains("button:"))
-                            conteudo = returnLineHtml(conteudo, "button"); 
+                        // Verifica se a linha contem a instrução para a geração de um botão
+                        if (contentLine.Contains("button:"))
+                            contentLine = returnLineHtml(contentLine, "button"); // Se contiver chama a função passando o conteudo até aqui e o tipo como "button"
+                        
+                        // Verifica se a linha contem a instrução para a geração de um input de texto
+                        if (contentLine.Contains("text:"))
+                            contentLine = returnLineHtml(contentLine, "text"); // Se contiver chama a função passando o conteudo até aqui e o tipo como "text"
 
+                        // Verifica se a linha contem a instrução para a geração de um input select
+                        if (contentLine.Contains("combobox:"))
+                            contentLine = returnLineHtml(contentLine, "combobox"); // Se contiver chama a função passando o conteudo até aqui e o tipo como "combobox"
+
+                        // Verifica se a linha contem a instrução para a geração de um input radio
+                        if (contentLine.Contains("radio:"))
+                            contentLine = returnLineHtml(contentLine, "radio"); // Se contiver chama a função passando o conteudo até aqui e o tipo como "radio"
                             
-                        // Verificar se a linha é uma tag de rótulo
-                        
-                        if (conteudo.Contains("label:"))
-                            conteudo = returnLineHtml(conteudo, "label"); 
-                        
-                        if (conteudo.Contains("text:"))
-                            conteudo = returnLineHtml(conteudo, "text"); 
-
+                        // Verifica se a linha contem a instrução para a geração de um input checkbox
+                        if (contentLine.Contains("checkbox:"))
+                            contentLine = returnLineHtml(contentLine, "checkbox"); // Se contiver chama a função passando o conteudo até aqui e o tipo como "checkbox"
                            
-                        if (conteudo.Contains("combobox:"))
-                            conteudo = returnLineHtml(conteudo, "combobox"); 
-
+                        writer.WriteLine(fixLineSpacement(contentLine)); // Chama função para corrigir espaços e escreve a linha no html
                         
-                        if (conteudo.Contains("radio:"))
-                            conteudo = returnLineHtml(conteudo, "radio"); 
-                            
-                        
-                        if (conteudo.Contains("checkbox:"))
-                            conteudo = returnLineHtml(conteudo, "checkbox"); 
-                           
-                        writer.WriteLine(fixLineSpacement(conteudo));
-                        writer.WriteLine("<br>");
+                        writer.WriteLine("<br>"); // Pula uma linha no html
                     }
 
                     // Escrever o fim do documento HTML
