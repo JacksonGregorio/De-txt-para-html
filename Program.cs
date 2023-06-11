4491df8908f4a51e7d3ae8c17b014bc39480487f
Jackson Gregório <98977689+JacksonGregorio@users.noreply.github.com>
2023-06-11 12:40:42 -0300

    Update Program.cs
    Adiconar o style do Nelson i Love you

--- Program.cs
+++ Program.cs
@@ -48,12 +48,13 @@ namespace projeto_nelson
                 using (StreamWriter writer = new StreamWriter(arquivoHtml))
                 {
                     // Escrever o início do documento HTML
-                    writer.WriteLine("<!DOCTYPE html>");
-                    writer.WriteLine("<html>");
-                    writer.WriteLine("<head>");
-                    writer.WriteLine("<title>Arquivo HTML gerado</title>");
-                    writer.WriteLine("</head>");
-                    writer.WriteLine("<body>");
+                 writer.WriteLine("<!DOCTYPE html>");
+                 writer.WriteLine("<html>");
+                 writer.WriteLine("<head>");
+                 writer.WriteLine("<title>Arquivo HTML gerado</title>");
+                 writer.WriteLine("<style>h1 {text-align: center; font-family: Arial, sans-serif; font-size: 48px; color: #FF0000; text-shadow: 2px 2px 4px #000000;} @keyframes fireworks-animation {0% {color: #FF0000; text-shadow: 2px 2px 4px #000000;} 14.285% {color: #FF8000; text-shadow: 2px 2px 4px #000000;} 28.57% {color: #FFFF00; text-shadow: 2px 2px 4px #000000;} 42.855% {color: #00FF00; text-shadow: 2px 2px 4px #000000;} 57.14% {color: #0000FF; text-shadow: 2px 2px 4px #000000;} 71.425% {color: #8000FF; text-shadow: 2px 2px 4px #000000;} 85.71% {color: #FF00FF; text-shadow: 2px 2px 4px #000000;} 100% {color: #FF0080; text-shadow: 2px 2px 4px #000000;}} h1.fireworks-animation {animation: fireworks-animation 2s infinite;}</style>");
+                 writer.WriteLine("</head>");
+                 writer.WriteLine("<body>");
 
                     // Iterar sobre as linhas do arquivo TXT
                     foreach (string linha in linhas)
